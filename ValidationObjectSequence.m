af56ee1315245f91b1ff45b4c38809da2f652cdc
Sammiege <sngeurts@gmail.com>
2017-10-31 10:49:51 +0100

    moved to SpotNGlia

--- ValidationObjectSequence.m
+++ ValidationObjectSequence.m
@@ -34,7 +34,19 @@ obj = obj.PreProcession
 
 obj = obj.ExtendedDeptOfField
 obj = obj.Registration
-
 obj = obj.BrainSegmentation
 
-obj = obj.SpotInfo
+obj = obj.SpotDetection
+
+%%
+temp = SpotNGlia(0)
+dr = dir([temp.SavePath,'/*.mat'])
+clear temp
+load([dr.folder,'/',dr.name])
+clear dr
+
+
+
+
+
+
