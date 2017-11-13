678ed782a3aed94ee95981d77eff63b59d35a538
Sammiege <sngeurts@gmail.com>
2017-11-13 16:18:57 +0100

    add batch 8-11

--- Big8BatchInfo.m
+++ Big8BatchInfo.m
@@ -1,12 +1,13 @@
 
 
 %%
-
+%{
 obj = obj.NewPath(1)
 
 filename = obj.SavePath
 infoname = obj.InfoName
 sngname = obj.SaveName
+%}
 
 P{1} = '/Volumes/Seagate Expansion Drive/PublicationBatch/2016-04-28 IL34 Csf1a Csf1b Bnc2 3 dpf/Csf1a B1'
 I{1} = 'INFO_Csf1a B1'
@@ -40,6 +41,46 @@ P{8} = '/Volumes/Seagate Expansion Drive/PublicationBatch/2017-02-03-p2rx3a-hcst
 I{8} = 'INFO_2017-02-03-p2rx3a-hcst-3dpf'
 S{8} = 'SNG_2017-02-03-p2rx3a-hcst-3dpf'
 
+P{9} = '/Volumes/Seagate Expansion Drive/PublicationBatch/2016-04-21 ch25h/Ch25h B2'
+I{9} = 'INFO_Ch25h B2'
+S{9} = 'SNG_Ch25h B2'
+
+P{10} = '/Volumes/Seagate Expansion Drive/PublicationBatch/2016-04-21 ch25h/WT B2'
+I{10} = 'INFO_WT B2'
+S{10} = 'SNG_WT B2'
+
+P{11} = '/Volumes/Seagate Expansion Drive/PublicationBatch/20171026_DKO_NR_3dpf'
+I{11} = 'INFO_20171026_DKO_NR_3dpf'
+S{11} = 'SNG_20171026_DKO_NR_3dpf'
+
+
+
+k1=1
+
+load([P{k1}, '/', S{k1}, '.mat'])
+
+obj = obj.NewPath(1)
+
+filename = obj.SavePath
+infoname = obj.InfoName
+sngname = obj.SaveName
+
+
+AlignedFish = imread([P{k1}, '/', 'AlignedFish', '/', obj.StackInfo(k1).stackname, '.tif']);
+
+
+
+sng_SaveCell2TiffStack(CorrectedFishTemp, [TempFolderName, '/', obj.StackInfo(k1).stackname, '.tif'])
+
+
+mfilename('fullpath')
+[folder, name, ext] = fileparts(which('obj'));
+
+S = dbstack('-completenames');
+S(1).file
+
+
+%{
 for k2 = 1:8
     
     load([obj.SavePath, '/', obj.InfoName, '.mat'], 'SpotsDetected')
@@ -56,7 +97,7 @@ for k2 = 1:8
     ds = cell2dataset([title; Sheet]);
     export(ds, 'file', [P{k2}, '/', I{k2}, '.csv'], 'delimiter', ',')
 end
-
+%}
 %{
  %writes to text file
  fileID = fopen([P{1},'/',I{1},'.txt'],'w');
