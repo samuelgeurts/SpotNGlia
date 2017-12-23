c76d43917452ad84d68ae95c375323b50079da41
Sammiege <sngeurts@gmail.com>
2017-12-23 01:59:15 +0100

    plot with maximum values of the histogram fish.
    show a bar plot with only the maximum values of the histogram of a singel fish. The value is averaged over the RGB band. The maximum is taken from the right site i.e. fishhead with the background removed.

---
+++ @SpotNGlia/ShowMaxFishHist.m
@@ -0,0 +1,16 @@
+function ShowMaxFishHist(obj)
+
+load([obj.SavePath, '/', obj.InfoName, '.mat'], 'RegistrationInfo')
+
+b = zeros(numel(RegistrationInfo),3);
+for k2=1:numel(RegistrationInfo)
+    b(k2, 1:3) = RegistrationInfo{k2}(find(strcmp({RegistrationInfo{k2}.name}, 'MaxFishColor'))).value;
+end
+bm = mean(b(:));
+
+figure; bar(mean(b, 2));
+        set(gca,'YLim',[0,265]);
+        hold on
+        line(get(gca, 'Xlim'),[bm, bm], 'color', [0, 0, 0]);  
+
+end
