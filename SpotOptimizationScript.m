fbb4fd9a0271771c34010b838a9304a536e7a00a
S.N. Geurts <sngeurts@gmail.com>
2017-12-23 13:22:18 +0100

    clean up

--- SpotOptimizationScript.m
+++ SpotOptimizationScript.m
@@ -3,40 +3,18 @@ obj = obj.NewPath(10)
 load([obj.SavePath, '/', 'SpotOptList', '.mat'], 'SpotOptList')
 
 %%
-CompleteTemplate = LoadTemplateSNG([obj.SourcePath, '/', 'Template 3 dpf']);
-
 clear zfinputlist
 %zfinputlist.ColorToGrayVectorL = {[-0.380803509067153;0.847820881126686;-0.369037181064066]};
-
-zfinputlist.ColorToGrayVectorL = {[0; 1; 0]};
-zfinputlist.ScaleBaseL = {0.333};
-zfinputlist.KthresholdL = {0};
-%zfinputlist.MPlevelsL = {4:6;5:7;6:8;4:7;5:8};
-zfinputlist.MPlevelsL = {6:11};
-
-%zfinputlist.MPthresholdL = {0.5 1 2 4 8 16 32 64};
-%zfinputlist.MPthresholdL = {1100 1200 1300 1400 1500 1600 1700};
-zfinputlist.MPthresholdL = {1450 1550 1650 1750};
-
-
-%{
-zfinputlist.MinSpotSizeL = {8.4084}
-zfinputlist.MaxSpotSizeL = {458}
-zfinputlist.MinProbabilityL = {0.0660}
-zfinputlist.MinSpotRange = 1
-zfinputlist.MaxSpotRange = 1
-zfinputlist.MinProbabilityRange = 1
-%}
 
 %{
 zfinputlist.ColorToGrayVectorL = {[0; 1; 0]};
 zfinputlist.ScaleBaseL = {0.5};
 zfinputlist.KthresholdL = {0};
 zfinputlist.MPlevelsL = {5:7};
-zfinputlist.MPthresholdL = {100};
+zfinputlist.MPthresholdL = {200};
 zfinputlist.MinSpotSizeL = {8.4084}
 zfinputlist.MaxSpotSizeL = {458}
-zfinputlist.MinProbabilityL = {0.03}
+zfinputlist.MinProbabilityL = {0.066}
 zfinputlist.MinSpotRange = 1
 zfinputlist.MaxSpotRange = 1
 zfinputlist.MinProbabilityRange = 1
@@ -46,17 +24,12 @@ zfinputlist.MinSpotRange = [0:0.01:0.06]
 zfinputlist.MaxSpotRange = [0.3:0.1:0.7]
 zfinputlist.MinProbabilityRange = [0:0.05:0.5]
 
-
-
-        
-obj2 = obj.SpotOptimization(1:50,zfinputlist)
+objTemp = obj.SpotOptimization(1:50,zfinputlist)
 
 %%
 
-    load([obj.SavePath, '/', 'SpotOptList', '.mat'], 'SpotOptList')
-
-
-    figure;plot([SpotOptList.MeanF1score])
+load([obj.SavePath, '/', 'SpotOptList', '.mat'], 'SpotOptList')
+figure;plot([SpotOptList.MeanF1score])
 
     
     %[SpotOptList.MeanF1score]
