d82f27e6eb23966b602f41ce3c3ea3ea126da9bc
Sammiege <sngeurts@gmail.com>
2018-01-15 22:02:02 +0100

    update relevant user methods

--- User Methods SpotNGlia.m
+++ User Methods SpotNGlia.m
@@ -1,18 +1,49 @@
-%select file location
-obj = SpotNGlia
 
+%% most important functions
+
+%create SpotNGlia object and select file location
+obj = SpotNGlia
 %compute stacks
 obj = obj.SliceCombination
 
 %check the following variables before proceeding
-
 openvar('obj.ImageInfo')
 openvar('obj.StackInfo')
 
-%run all algorithms and save final images
-%when preprocession is completed, half of the time is passed
-
+%run all algorithms in order and saves in between
 obj = obj.CompleteProgram
 
-%Show fish and correct brainfishbrains
-obj.CheckBrain
+%Show fish and apply braincorrections and spotcorrections
+obj.CheckFish
+
+%% individual algorithm functions which has to be processed in order
+
+obj = obj.PreProcession
+obj = obj.ExtendedDeptOfField
+obj = obj.Registration
+obj = obj.BrainSegmentation
+obj = obj.SpotDetection
+
+%% supporting properties that can be set after the SpotNglia object is created
+
+%dont show screen that alerts for stackinfo and imageinfo check [true or false]
+obj.ImageInfoChecked_TF = true
+%preset obj.Sorting ['Date' or 'Name']
+obj.Sorting = 'Date'
+%change delimiter to [';' or ',']
+obj.Delimiter = ';' 
+
+%% run multiple batches
+
+for k = 1:5
+    obj{k} = SpotNGlia
+    obj{k}.ImageInfoChecked_TF = true
+    obj{k}.Sorting = 'Date'
+    obj{k} = obj{k}.SliceCombination
+end
+
+for k = 1:5
+   obj{k} = obj{k}.CompleteProgram    
+end
+
+
